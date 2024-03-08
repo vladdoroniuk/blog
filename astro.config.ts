c88d586a3072ebf8aff4bfc8250d526ccbe9c600
vladdoroniuk <vladyslav.doroniuk@gmail.com>
2024-03-08 20:33:49 +0200

    chore: add netlify support

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 import rehypeExternalLinks from "rehype-external-links";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -47,4 +48,8 @@ export default defineConfig({
     },
   },
   scopedStyleStrategy: "where",
+  output: "server",
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
 });
